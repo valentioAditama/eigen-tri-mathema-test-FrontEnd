7cf16ade6dc8d83c3fc18eefe3738a4e8feefb63
valentioAditama <valentioaditama8@gmail.com>
2023-02-02 02:01:21 +0700

    update algorithm

---
+++ algorithm/reverse.js
@@ -0,0 +1,7 @@
+var alphabet = "NEGIE1";
+var reverse = "";
+for (var _i = 0, alphabet_1 = alphabet; _i < alphabet_1.length; _i++) {
+    var char = alphabet_1[_i];
+    reverse = char + reverse;
+}
+console.log(alphabet);
---
+++ algorithm/reverse.ts
@@ -0,0 +1,9 @@
+let alphabet: string = "NEGIE1";
+
+let reverse: string = "";
+
+for (let char of alphabet){
+    reverse = char + reverse;
+}
+
+console.log(alphabet);
