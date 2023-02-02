bcb7ad1b1e5f6a7ca04a8948907dabebcc73d7a0
valentioAditama <valentioaditama8@gmail.com>
2023-02-02 21:35:46 +0700

    Update

--- algorithm/latest.js
+++ algorithm/latest.js
@@ -2,6 +2,6 @@ function longest(sentence) {
     var seacrhMax = sentence.split(" ", 50);
     var searchChar = seacrhMax.sort(function (a, b) { return b.length - a.length; })[0];
     var calculate = seacrhMax.map(function (seacrhMax) { return seacrhMax.length; });
-    return searchChar + ": " + Math.max.apply(Math, calculate) + " Character";
+    console.log(searchChar + ": " + Math.max.apply(Math, calculate) + " Character");
 }
-console.log(longest("Saya sangat senang mengerjakan soal algoritma"));
+longest("Saya sangat senang mengerjakan soal algoritma");
--- algorithm/latest.ts
+++ algorithm/latest.ts
@@ -1,10 +1,9 @@
-function longest(sentence: string){
+function longest(sentence: string) {
     const seacrhMax = sentence.split(" ", 50)
     const searchChar = seacrhMax.sort((a, b) => b.length - a.length)[0]
     const calculate = seacrhMax.map(seacrhMax => seacrhMax.length);
 
-    return searchChar + ": " + Math.max(...calculate) + " Character"
+    console.log(searchChar + ": " + Math.max(...calculate) + " Character")
 }
 
-console.log(longest("Saya sangat senang mengerjakan soal algoritma"))
-
+longest("Saya sangat senang mengerjakan soal algoritma")
